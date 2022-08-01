54277d8807753ddcfc612e0d006f12d3230cdf8f
jingjingxyk <zonghengbaihe521@qq.com>
2022-08-02 01:09:11 +0800

    save change

--- firefox.sh
+++ firefox.sh
@@ -4,12 +4,15 @@ set -exu
 
 __DIR__=$(cd "$(dirname "$0")";pwd)
 cd ${__DIR__}
+__ROOT__=$(cd ${__DIR__}/../;pwd)
 
+cd ${__ROOT__}
 uuid=$(cat /proc/sys/kernel/random/uuid)
 user_data_dir="/tmp/${uuid}"
 if [ ! -d $user_data_dir ] ;then
-  mkdir $user_data_dir
+  mkdir -p  $user_data_dir
 fi
 
+cd ${__DIR__}
 
-./firefox --profile $dir https://www.marxists.org/ https://stackoverflow.com/tags/socat/hot?filter=all
+firefox --profile $user_data_dir https://www.marxists.org/ https://stackoverflow.com/tags/socat/hot?filter=all
