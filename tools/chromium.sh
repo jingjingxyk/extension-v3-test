a8e208498cbe26a5640c7a935b9f2a2306b05d87
jingjingxyk <zonghengbaihe521@qq.com>
2022-08-06 07:09:51 +0800

    rename file

--- tools/chromium.sh
+++ tools/chromium.sh
@@ -5,6 +5,9 @@ set -exu
 __DIR__=$(cd "$(dirname "$0")";pwd)
 cd ${__DIR__}
 
+__ROOT__=$(cd ${__DIR__}/../;pwd)
+
+
 uuid=$(cat /proc/sys/kernel/random/uuid)
 user_data_dir="/tmp/${uuid}"
 if [ ! -d $user_data_dir ] ;then
@@ -14,6 +17,7 @@ export GOOGLE_API_KEY="no"
 export GOOGLE_DEFAULT_CLIENT_ID="no"
 export GOOGLE_DEFAULT_CLIENT_SECRET="no"
 
+extension=${__ROOT__}
 ${__DIR__}/chromium-browser/chromium/chrome \
 --user-data-dir=$user_data_dir \
 --show-app-list \
@@ -21,6 +25,15 @@ ${__DIR__}/chromium-browser/chromium/chrome \
 --enable-remote-extensions  \
 --enable-extensions \
 --auto-open-devtools-for-tabs \
---load-extension=${__DIR__}/ \
+--load-extension=${extension}/ \
+--enable-logging=stderr --v=1 \
+--remote-debugging-port=9221 \
 https://www.marxists.org/
 
+
+:<<\EOF
+
+Chrome 104 支持硬解 HEVC 了
+添加下面这个启动参数就可以了 open /Applications/Google\ Chrome.app --args --enable-features=PlatformHEVCDecoderSupport
+
+EOF
