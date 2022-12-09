68fe283ba46a883dc6bb58f5c36208170907ab78
jingjingxyk <zonghengbaihe521@qq.com>
2022-12-09 23:53:20 +0800

    save changed

--- tools/firefox.sh
+++ tools/firefox.sh
@@ -4,13 +4,21 @@ set -exu
 
 __DIR__=$(cd "$(dirname "$0")";pwd)
 cd ${__DIR__}
-__ROOT__=$(cd ${__DIR__}/../;pwd)
 
-cd ${__ROOT__}
 uuid=$(cat /proc/sys/kernel/random/uuid)
-user_data_dir="/tmp/${uuid}"
-mkdir -p  $user_data_dir
+profile_folder="/tmp/${uuid}"
+
+mkdir -p  $profile_folder
 
 cd ${__DIR__}
 
-firefox --profile $user_data_dir https://www.marxists.org/ https://stackoverflow.com/tags/socat/hot?filter=all
+
+# https://wiki.mozilla.org/Firefox/CommandLineOptions
+
+
+./firefox/firefox \
+-profile $profile_folder \
+-devtools \
+-jsconsole \
+-start-debugger-server 9221 \
+https://www.marxists.org/ https://stackoverflow.com/tags/socat/hot?filter=all
